f83277dec09517571a8fde5c34fada718016a665
tonmar007 <tonmar005@gmail.com>
2022-06-03 20:54:55 +0200

    Connecting app with firebase

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,63 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./Header";
 import Order from "./Order";
 import Inventory from "./Inventory";
+import sampleFishes from "../sample-fishes";
+import Fish from "./Fish";
+import { useParams } from "react-router-dom";
+import { db } from "../base";
+import { doc, setDoc, deleteDoc } from "firebase/firestore"; 
 
 function App() {
-  const [fishes, setFishes] = useState([]);
-  const [order, setOrder] = useState("");
+  const [fishes, setFishes] = useState({});
+  const [order, setOrder] = useState({});
+  const { storeId } = useParams();
+
+  useEffect(() => {
+    deleteDoc(doc(db, "sparkling-angry-leaves", "fishes"));
+    setDoc(doc(db, `${storeId}`, "fishes"), fishes);
+  }, [fishes]);
 
   const addFish = fish => {
-    setFishes([ ...fishes, fish ]);
+    setFishes({ ...fishes, fish });
   };
+
+  const loadSampleFishes = () => {
+    setFishes(sampleFishes);
+  }
+
+  const addToOrder = (key) => {
+    const orderCopy = { ...order};
+    orderCopy[key] = orderCopy[key] + 1 || 1;
+    setOrder(orderCopy);
+  }
 
   return (
     <div className="catch-of-the-day">
       <div className="menu">
         <Header tagline="Fresh Seafood Market" />
+        <ul className="fishes">
+          {
+            Object.keys(fishes).map(key => (
+            <Fish 
+              key={key}
+              index={key}
+              fish={fishes[key]}
+              addToOrder={addToOrder}
+            />
+            ))
+            //fishes.map(fish => <Fish key={fish} />)
+          }
+        </ul>
       </div>
-      <Order />
-      <Inventory addFish={addFish} />
+      <Order 
+        fishes={fishes}
+        order={order}
+      />
+      <Inventory 
+        addFish={addFish} 
+        loadSampleFishes={loadSampleFishes}
+      />
     </div>
   );
 }
--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,10 +1,11 @@
 import AddFishForm from "./AddFishForm";
 
-function Inventory({addFish}) {
+function Inventory({addFish, loadSampleFishes}) {
   return (
     <div className="inventory">
       <h2>Inventory</h2>
       <AddFishForm addFish={addFish} />
+      <button onClick={loadSampleFishes}>Load Sample Fishes</button>
     </div>
   );
 }
